89057c54a2351e1ef2c65a64c071b7733503f0ce
jcm300 <zeclmartins@gmail.com>
2019-06-07 14:41:39 +0100

    cleaning code

--- getValsFromIozone.py
+++ getValsFromIozone.py
@@ -15,10 +15,8 @@ reread = []
 random_read = []
 random_write = []
 
-linesN = 0
 for line in lines:
     if re.match(r"^\s+([0-9]+\s+)+$", line) :
-        linesN += 1
         numbers = re.split(r"\s+",line)
         numbers = numbers[1:-1]
         numbers = list(map(int, numbers))
@@ -31,20 +29,11 @@ for line in lines:
         random_read.append(numbers[6])
         random_write.append(numbers[7])
 
-kb = statistics.median(kb)
-reclen = statistics.median(reclen)
-write = statistics.median(write)
-rewrite = statistics.median(rewrite)
-read = statistics.median(read)
-reread = statistics.median(reread)
-random_read = statistics.median(random_read)
-random_write = statistics.median(random_write)
-
-print(kb)
-print(reclen)
-print(write)
-print(rewrite)
-print(read)
-print(reread)
-print(random_read)
-print(random_write)
+print(statistics.median(kb))
+print(statistics.median(reclen))
+print(statistics.median(write))
+print(statistics.median(rewrite))
+print(statistics.median(read))
+print(statistics.median(reread))
+print(statistics.median(random_read))
+print(statistics.median(random_write))
