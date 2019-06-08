0da7dd07a58eb94122a644b0b8465dbe96432593
MQuaresma <miguelquaresma97@gmail.com>
2019-06-08 15:38:12 +0100

    Improvements on DTrace script

--- io_tracing.d
+++ io_tracing.d
@@ -2,54 +2,65 @@
 
 #pragma D option quiet
 
+dtrace:::BEGIN
+{
+    read_delta = 0;
+    random_read_delta = 0;
+    write_delta = 0;
+    random_write_delta = 0;
+}
+
+
+syscall::read:entry,
 syscall::write:entry
 {
     self->entry = timestamp;
 }
 
 //initiate non continuous IO op
-syscall::lseek:entry
+syscall::lseek:return
 {
     self->random_io = 1;
 }
 
 syscall::write:return
+/self->entry/
 {
     r_time = (timestamp-self->entry);
     if(self->random_io == 1)
     {
-        self->random_io = 0; 
-        @random_write_rate = avg(arg0/r_time);
+        self->random_io = 0;
+        random_write_delta += r_time;
+        @random_write_rate = sum(arg0);
     }
     else
-        @write_rate = avg(arg0/r_time);
-}
-
-syscall::read:entry
-{
-    self->entry = timestamp;
+    {
+        write_delta += r_time;
+        @write_rate = sum(arg0);
+    }
 }
 
 syscall::read:return
+/self->entry/
 {
     r_time = (timestamp-self->entry);
     if(self->random_io == 1)
     {
-        self->random_io = 0; 
-        @random_read_rate = avg(arg0/r_time);
+        self->random_io = 0;
+        random_read_delta += r_time;
+        @random_read_rate = sum(arg0);
     }
     else
-        @read_rate = avg(arg0/r_time);
+    {
+        read_delta += r_time;
+        @read_rate = sum(arg0);
+    }
 }
 
 dtrace:::END
 {
-    printf("Read rate");
-    printa(@read_rate);
-    printf("Write rate");
-    printa(@write_rate);
-    printf("Random Read rate");
-    printa(@random_read_rate);
-    printf("Random Write rate");
-    printa(@random_write_rate)
+    normalize(@read_rate, read_delta/1000000000);
+    normalize(@write_rate, write_delta/1000000000);
+    normalize(@random_read_rate, random_read_delta/1000000000);
+    normalize(@random_write_rate, random_write_delta/1000000000);
 }
